3ea7238d02907079d1d03c50e73d0abb62b10c1c
Saybal <roysaybal@gmail.com>
2025-08-10 17:02:03 +0600

    Update Featured_sec for dark mode

--- Client Site/src/Shared/Components/Featured Section/Featured_sec.jsx
+++ Client Site/src/Shared/Components/Featured Section/Featured_sec.jsx
@@ -3,6 +3,7 @@ import Swal from "sweetalert2";
 import Loading from "../Loading";
 import Featured_card from "./Featured_card";
 import { Link } from "react-router";
+import { useTheme } from "../../Hooks/useTheme";
 
 const Featured_sec = () => {
   const [allArtifacts, setAllArtifacts] = useState([]);
@@ -10,7 +11,7 @@ const Featured_sec = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetch("https://artifacts-tracker-sable.vercel.app/all-artifacts")
+    fetch("http://localhost:3000/all-artifacts")
       .then((res) => res.json())
       .then((data) => {
         setAllArtifacts(data);
@@ -27,23 +28,25 @@ const Featured_sec = () => {
         });
       });
   }, []);
+
+  const currentTheme = useTheme();
   return (
-    <div className="min-w-screen bg-[#ECE7E1] pb-32 px-3 md:px-8 lg:px-0">
+    <div className={`min-w-screen  ${currentTheme === 'acid' ? "bg-[#ECE7E1]" : "bg-[#1E1B18]"} pb-12 px-3 md:px-8 lg:px-0`}>
       <div className="max-w-7xl mx-auto">
         {loading ? (
           <Loading />
         ) : (
           <>
             <div className="mt-20 bg-bg-none">
-              <p className="title-font text-lg md:text-xl font-semibold">
+              <p className={`title-font text-lg md:text-xl font-bold ${currentTheme === 'acid' ? "text-[#960018]" : "text-[#D72638]"}`}>
                 Featured Artifacts
               </p>
-              <h2 className="title-font text-3xl md:text-5xl font-bold">
+              <h2 className={`title-font text-3xl md:text-5xl font-bold ${currentTheme === 'sunset' ? "text-[#E4DAD3]" : "text-black"} `}>
                 Glimpses Into the Golden Ages <br /> Through Beloved Artifacts
               </h2>
             </div>
 
-            <div className=" bg-[#F5F2EB] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-10">
+            <div className={` ${currentTheme === 'acid' ? "bg-[#F5F2EB]" : "bg-[#1E1B18]"} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-10`}>
               {allArtifacts.slice(0, 4).map((artifact, index) => (
                 <Featured_card key={index} artifact={artifact} />
               ))}
@@ -51,7 +54,7 @@ const Featured_sec = () => {
             <div className="mt-5 w-full flex justify-center px-4 sm:px-6 md:px-10">
               <Link
                 to="/all-artifacts"
-                className="btn border-2 bg-transparent border-[#800020] text-[#800020] text-base sm:text-lg md:text-xl normal-font font-semibold py-2 sm:py-3 md:py-4 px-6 sm:px-8 md:px-10 rounded-xl hover:font-bold hover:border-[3px]"
+                className={`btn border-2 bg-transparent ${ currentTheme === 'acid' ? "border-[#800020] text-[#800020]" : "border-[#D72638] text-[#D72638]" }  text-base sm:text-lg md:text-xl normal-font font-semibold py-2 sm:py-3 md:py-4 px-6 sm:px-8 md:px-10 rounded-xl hover:font-bold hover:border-[3px]`}
               >
                 See All Artifacts
               </Link>

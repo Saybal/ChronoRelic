b3a1a04d12b83742a48dbd289d69db6e85895aba
Saybal <roysaybal@gmail.com>
2025-08-10 14:40:51 +0600

    Update ArtifactOfTheWeekContainer.jsx

--- Client Site/src/Shared/Components/Artifact of the Week/ArtifactOfTheWeekContainer.jsx
+++ Client Site/src/Shared/Components/Artifact of the Week/ArtifactOfTheWeekContainer.jsx
@@ -7,10 +7,10 @@ const ArtifactOfTheWeekContainer = () => {
       <div className="max-w-7xl mx-auto mt-20">
         <div className="p-4 md:p-7 lg:p-0">
           <h2 className="text-lg md:text-xl  font-medium title-font font-serif">
-            From the Curator’s Desk
+            This Week in Time
           </h2>
           <h1 className="title-font text-3xl md:text-5xl font-bold mt-2">
-            Whispers of Forgotten Worlds <br /> Rekindled Anew
+            A rare find from the past week, handpicked <br/> for its story and charm.
           </h1>
         </div>
 
@@ -21,5 +21,6 @@ const ArtifactOfTheWeekContainer = () => {
     </div>
     );
 };
+
 
 export default ArtifactOfTheWeekContainer;
